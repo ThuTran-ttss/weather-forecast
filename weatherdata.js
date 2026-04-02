4d93240bad768d3b1b1e42771cb58c834929639d
Thư Trần <minhthu.tranle0612@gmail.com>
2026-04-02 12:59:21 +0700

    Save local challenge files before pulling

--- weatherdata.js
+++ weatherdata.js
@@ -11,13 +11,29 @@ var highTemperatures = [55, 57, 53, 47, 56, 50];
 var lowTemperatures = [33, 30, 29, 35, 31, 32]; 
 console.log(highTemperatures[0]);
 console.log(lowTemperatures[5]);
+// 3. What is the average high temperature from the data set?
 let sumHighTemp = 0;
 for (let i = 0; i < highTemperatures.length; i++){
     sumHighTemp+= highTemperatures[i];
 }
 console.log(`Average high temperature from the data set is : ${sumHighTemp/highTemperatures.length}`);
+// 4. What is the average low temperature from the data set?
 let sumLowTemp = 0;
 for (let k = 0; k < lowTemperatures.length; k++){
     sumLowTemp+= lowTemperatures[k];
 }
 console.log(`Average low temperature from the data set is : ${(sumLowTemp/lowTemperatures.length).toFixed(2)}`);
+// 5. What is the median high temperature from the data set?
+highTemperatures.sort((a,b) => a -b)
+let midIndex = parseInt(highTemperatures.length/2);
+let median = 0;
+if (highTemperatures.length%2 === 0) {median =( highTemperatures[midIndex-1] + highTemperatures[midIndex])/2;}
+else {median = highTemperatures[midIndex];}
+console.log(median);
+// 6. What is the median low temperature from the data set?
+lowTemperatures.sort((a,b) => a -b)
+let midIndexLow = parseInt(lowTemperatures.length/2);
+let medianLow = 0;
+if (lowTemperatures.length%2 === 0) {medianLow =(lowTemperatures[midIndexLow-1] + lowTemperatures[midIndexLow])/2;}
+else {medianLow = lowTemperatures[midIndexLow];}
+console.log(medianLow);
---
+++ weatherobject.js
@@ -0,0 +1,13 @@
+// In your JavaScript file, create an object that represents the current weather. Your can reuse your data from the first exercise. Your object should have the following properties:
+
+// temperature
+// conditions
+// Log the current temperature to the console.
+
+// Use string template literals to log a sentence about the weather console. (Example: "It's 64 degrees F and sunny.")
+let currentWeather = {
+temperature: 34,
+conditions : "mostly sunny"
+};
+console.log(currentWeather.temperature);
+console.log(`It's ${currentWeather.temperature} degrees Celcius and ${currentWeather.conditions}.`)
